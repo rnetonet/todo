9e259ec19e78f49edf7459090c483ad529a09faf
rnetonet <rneto@rneto.net>
2025-01-10 12:03:15 -0300

    endpoint listarTodos e renomear entidades

--- src/main/java/br/ufba/tomorrow/todo/api/TodoController.java
+++ src/main/java/br/ufba/tomorrow/todo/api/TodoController.java
@@ -1,5 +1,7 @@
 package br.ufba.tomorrow.todo.api;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -14,8 +16,6 @@ import org.springframework.web.bind.annotation.RestController;
 import br.ufba.tomorrow.todo.domain.Todo;
 import br.ufba.tomorrow.todo.dto.TodoCriarDTO;
 import br.ufba.tomorrow.todo.dto.TodoDTO;
-import br.ufba.tomorrow.todo.mappers.TodoMapper;
-import br.ufba.tomorrow.todo.repository.TodoRepository;
 import br.ufba.tomorrow.todo.services.TodoService;
 
 @RestController
@@ -24,18 +24,18 @@ public class TodoController {
     @Autowired
     TodoService todoService;
 
-    @GetMapping("/todos")
-    public ResponseEntity<?> listar() {
-        return null;
+    @GetMapping("/listar-todos")
+    public ResponseEntity<List<TodoDTO>> listarTodos() {
+        return ResponseEntity.ok(todoService.listarTodos());
     }
 
-    @GetMapping("/{userId}")
+    @GetMapping("/listar-por-usuario/{userId}")
     public ResponseEntity<?> listarPorUsuario(@PathVariable long userId) {
         return null;
     }
 
 
-    @GetMapping("/{userId}/{status}")
+    @GetMapping("/listar-por-usuario/{userId}/status/{status}")
     public ResponseEntity<?> listarPorUsuarioEStatus(@PathVariable long userId, @PathVariable String status) {
         return null;
     }
--- src/main/java/br/ufba/tomorrow/todo/services/TodoService.java
+++ src/main/java/br/ufba/tomorrow/todo/services/TodoService.java
@@ -1,5 +1,8 @@
 package br.ufba.tomorrow.todo.services;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -21,5 +24,11 @@ public class TodoService {
         TodoDTO todoSalvoDTO = TodoMapper.INSTANCE.toDTO(todoSalvo);
 
         return todoSalvoDTO;
+    }
+
+    public List<TodoDTO> listarTodos() {
+        List<TodoDTO> todos = todoRepository.findAll().stream().map(TodoMapper.INSTANCE::toDTO)
+                .collect(Collectors.toList());
+        return todos;
     }
 }
